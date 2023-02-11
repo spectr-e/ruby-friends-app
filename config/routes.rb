3e4d7c1807cfea553ba223edc9d71bef8238cd24
spectr-e <devs.josia@gmail.com>
2023-02-11 15:38:01 +0300

    Created a database with CRUD functionality + navb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  resources :friends
   root "home#index"
   get 'home/about'
 end
